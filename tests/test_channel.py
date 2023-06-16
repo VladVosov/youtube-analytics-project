8e5cc2f5801e0d7cb8fbcb330a93d1ead5093bde
vlad_vosov <vlad.shvetesov@gmail.com>
2023-06-16 21:26:09 +1000

    HomeWork#3

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,8 @@
 from src.channel import Channel
 
+def test_str():
+    channel1 = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+    assert str(channel1) == "MoscowPython (https://yt3.ggpht.com/ytc/AGIKgqNv2rZ6mOSuXvJLYhmTc0nd-LtI5RiDtsEBpguJXA=s88-c-k-c0x00ffffff-no-rj)"
 
 
     
