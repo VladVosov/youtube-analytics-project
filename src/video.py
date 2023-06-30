4736c6801c7c1393819d6366e7350dcbcba92fa4
vlad_vosov <vlad.shvetesov@gmail.com>
2023-06-30 22:33:05 +1000

    HomeWork#4 Corrected

--- src/video.py
+++ src/video.py
@@ -11,12 +11,25 @@ class Video:
 
     def __init__(self, id_video):
         self.id_video = id_video
-        self.video = self.youtube.videos().list(id=self.id_video, part='snippet,statistics').execute()
-        self.id = self.video['items'][0]['id']
-        self.name = self.video['items'][0]['snippet']['title']
-        self.url = self.video['items'][0]['snippet']['thumbnails']['standard']['url']
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        video_response = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',id=id_video).execute()
+        self.title: str = video_response['items'][0]['snippet']['title']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+
+    def __str__(self):
+        return self.title
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+
+    @staticmethod
+    def __printj(dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 
 
 class PLVideo(Video):
@@ -29,6 +42,4 @@ class PLVideo(Video):
         self.id = self.playlist_items['items'][0]['id']
         self.title = self.playlist_items['items'][0]['snippet']['title']
         self.url = f"https://youtu.be/{id_video}"
-        self.view_count = self.video["items"][0]["statistics"]["viewCount"]
-        self.like_count = self.video["items"][0]["statistics"]["likeCount"]
         self.id_PL = self.playlist_response['items'][0]['id']
